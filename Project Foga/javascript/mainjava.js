c573d72662261840ea7473aeaccc7f21cbc3971a
Salih <salihyakuphankeskin@gmail.com>
2021-12-26 10:21:50 +0300

    init

---
+++ Project Foga/javascript/mainjava.js
@@ -0,0 +1,8 @@
+console.log("Welcome to GeeksforGeeks");
+// document.write(document.getElementsByClassName("FormDataText").innerHTML);
+
+
+// Ekrana yazı yazdırmak 
+//document.write("İlk javascript");
+
+//alert
